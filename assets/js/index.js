0417c2708ca1b79daff29905a4494c453aa6b8c0
kaushlendrakumar <designerkaushlendra@gmail.com>
2018-03-10 00:26:24 +0530

    first push

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+var countDownDate = new Date("March 15, 2018 15:37:25").getTime();
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    
+    var distance = countDownDate - now;
+   	
+   	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("timer-wrapper").innerHTML = days + "Day  " + hours + "HR "
+    + minutes + "Min " + seconds + "Sec ";
+    
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer-wrapper").innerHTML = "EXPIRED";
+    }
+}, 1000);
